6cbebe1aec864fc061588fd67d0bb71671be8ff0
Caleb Reiter <ctr020196@gmail.com>
2020-04-20 21:15:40 -0500

    :tada: initialize project

---
+++ b-118/b-118/Program.cs
@@ -0,0 +1,45 @@
+﻿using DSharpPlus;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+
+namespace b_118
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static string token;
+
+        static void Main(string[] args)
+        {
+            var environmentName = Environment.GetEnvironmentVariable("ENV");
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile($"appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{environmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            var configuration = builder.Build();
+            token = configuration.GetSection("Token")["B-118"];
+
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = token,
+                TokenType = TokenType.Bot
+            });
+
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong");
+            };
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
