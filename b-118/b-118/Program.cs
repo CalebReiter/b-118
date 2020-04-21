6ff3f43fda4760ae55f90bce265f84b815d83591
Caleb Reiter <ctr020196@gmail.com>
2020-04-21 00:07:09 -0500

    Squashed commit of the following:
    commit 8405c1588d4708eb54ddbde9d7f1d21478c9c534
    Author: Caleb Reiter <ctr020196@gmail.com>
    Date:   Tue Apr 21 00:00:40 2020 -0500
    
        :sparkles: set up logging
    
    commit 3fbcb6fe16a0d45cadec05f6a09194854b2816c9
    Author: Caleb Reiter <ctr020196@gmail.com>
    Date:   Mon Apr 20 23:59:34 2020 -0500
    
        :heavy_plus_sign: Add Logging dependencies.
    
    :heavy_plus_sign: add logging dependencies.
    :sparkles: set up logging

--- b-118/b-118/Program.cs
+++ b-118/b-118/Program.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace b_118
 {
@@ -9,10 +11,11 @@ namespace b_118
     {
         static DiscordClient discord;
         static string token;
+        static DSharpPlus.LogLevel logLevel;
 
         static void Main(string[] args)
         {
-            var environmentName = Environment.GetEnvironmentVariable("ENV");
+            var environmentName = Environment.GetEnvironmentVariable("ENVIRONMENT") ?? "Production";
 
             var builder = new ConfigurationBuilder()
                 .AddJsonFile($"appsettings.json", true, true)
@@ -21,14 +24,58 @@ namespace b_118
 
             var configuration = builder.Build();
             token = configuration.GetSection("Token")["B-118"];
+            switch (configuration.GetSection("Log")["Level"])
+            {
+                case "Debug":
+                    {
+                        logLevel = DSharpPlus.LogLevel.Debug;
+                        break;
+                    }
+                case "Info":
+                    {
+                        logLevel = DSharpPlus.LogLevel.Info;
+                        break;
+                    }
+                case "Warning":
+                    {
+                        logLevel = DSharpPlus.LogLevel.Warning;
+                        break;
+                    }
+                case "Error":
+                    {
+                        logLevel = DSharpPlus.LogLevel.Error;
+                        break;
+                    }
+                case "Critical":
+                    {
+                        logLevel = DSharpPlus.LogLevel.Critical;
+                        break;
+                    }
+                default:
+                    {
+                        logLevel = DSharpPlus.LogLevel.Info;
+                        break;
+                    }
+            }
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole());
         }
 
         static async Task MainAsync(string[] args)
         {
             discord = new DiscordClient(new DiscordConfiguration
             {
+                UseInternalLogHandler = true,
+                LogLevel = logLevel,
                 Token = token,
                 TokenType = TokenType.Bot
             });
